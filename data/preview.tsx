81f2ac443af34e391e549414b5fed8543bff9673
Yarikl_08 <igoshinyaromir@mail.ru>
2024-01-17 17:42:45 +0300

    Initial commit

---
+++ data/preview.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="preview" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="3366" columns="66">
+ <image source="../../../../../Program Files/Tiled/examples/sticker-knight/preview.png" width="2200" height="1700"/>
+ <wangsets>
+  <wangset name="Рыцари" type="mixed" tile="2604">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
---
+++ data/tmw_desert_spacing.tsx
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
+ <image source="../../../../../Program Files/Tiled/examples/tmw_desert_spacing.png" width="265" height="199"/>
+ <tile id="47" probability="50"/>
+ <wangsets>
+  <wangset name="the desert area пустынная местность" type="mixed" tile="14">
+   <wangcolor name="cobblestone булыжник" color="#ffff00" tile="9" probability="1"/>
+   <wangcolor name="sand песок" color="#00ff00" tile="29" probability="1"/>
+   <wangcolor name="pavement тротуар" color="#0000ff" tile="33" probability="1"/>
+   <wangcolor name="dirt земля почва" color="#ff7700" tile="14" probability="1"/>
+   <wangcolor name="декоративные плитки" color="#555500" tile="38" probability="25"/>
+   <wangcolor name="указатель выхода" color="#ff00d8" tile="45" probability="50"/>
+   <wangtile tileid="0" wangid="2,2,0,1,0,2,2,2"/>
+   <wangtile tileid="1" wangid="2,2,0,1,1,1,0,2"/>
+   <wangtile tileid="2" wangid="2,2,2,2,0,1,0,2"/>
+   <wangtile tileid="3" wangid="4,4,2,2,2,4,4,4"/>
+   <wangtile tileid="4" wangid="4,4,4,4,2,2,2,4"/>
+   <wangtile tileid="5" wangid="2,2,0,4,0,2,2,2"/>
+   <wangtile tileid="6" wangid="2,2,0,4,4,4,0,2"/>
+   <wangtile tileid="7" wangid="2,2,2,2,0,4,0,2"/>
+   <wangtile tileid="8" wangid="0,1,1,1,0,2,2,2"/>
+   <wangtile tileid="9" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="10" wangid="0,2,2,2,0,1,1,1"/>
+   <wangtile tileid="11" wangid="2,2,2,4,4,4,4,4"/>
+   <wangtile tileid="12" wangid="2,4,4,4,4,4,2,2"/>
+   <wangtile tileid="13" wangid="0,4,4,4,0,2,2,2"/>
+   <wangtile tileid="14" wangid="4,4,4,4,4,4,4,4"/>
+   <wangtile tileid="15" wangid="0,2,2,2,0,4,4,4"/>
+   <wangtile tileid="16" wangid="0,1,0,2,2,2,2,2"/>
+   <wangtile tileid="17" wangid="1,1,0,2,2,2,0,1"/>
+   <wangtile tileid="18" wangid="0,2,2,2,2,2,0,1"/>
+   <wangtile tileid="19" wangid="1,1,0,2,0,1,1,1"/>
+   <wangtile tileid="20" wangid="1,1,1,1,0,2,0,1"/>
+   <wangtile tileid="21" wangid="0,4,0,2,2,2,2,2"/>
+   <wangtile tileid="22" wangid="4,4,0,2,2,2,0,4"/>
+   <wangtile tileid="23" wangid="0,2,2,2,2,2,0,4"/>
+   <wangtile tileid="24" wangid="2,2,0,3,0,2,2,2"/>
+   <wangtile tileid="25" wangid="2,2,0,3,3,3,0,2"/>
+   <wangtile tileid="26" wangid="2,2,2,2,0,3,0,2"/>
+   <wangtile tileid="27" wangid="0,2,0,1,1,1,1,1"/>
+   <wangtile tileid="28" wangid="0,1,1,1,1,1,0,2"/>
+   <wangtile tileid="29" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="30" wangid="5,5,5,5,5,5,5,5"/>
+   <wangtile tileid="31" wangid="5,5,5,5,5,5,5,5"/>
+   <wangtile tileid="32" wangid="0,3,3,3,0,2,2,2"/>
+   <wangtile tileid="33" wangid="3,3,3,3,3,3,3,3"/>
+   <wangtile tileid="34" wangid="0,2,2,2,0,3,3,3"/>
+   <wangtile tileid="35" wangid="3,3,0,2,0,3,3,3"/>
+   <wangtile tileid="36" wangid="3,3,3,3,0,2,0,3"/>
+   <wangtile tileid="37" wangid="5,5,5,5,5,5,5,5"/>
+   <wangtile tileid="38" wangid="5,5,5,5,5,5,5,5"/>
+   <wangtile tileid="39" wangid="5,5,5,5,5,5,5,5"/>
+   <wangtile tileid="40" wangid="0,3,0,2,2,2,2,2"/>
+   <wangtile tileid="41" wangid="3,3,0,2,2,2,0,3"/>
+   <wangtile tileid="42" wangid="0,2,2,2,2,2,0,3"/>
+   <wangtile tileid="43" wangid="0,2,0,3,3,3,3,3"/>
+   <wangtile tileid="44" wangid="0,3,3,3,3,3,0,2"/>
+   <wangtile tileid="45" wangid="6,6,6,6,6,6,6,6"/>
+   <wangtile tileid="46" wangid="5,5,5,5,5,5,5,5"/>
+   <wangtile tileid="47" wangid="5,5,5,5,5,5,5,5"/>
+  </wangset>
+ </wangsets>
+</tileset>
